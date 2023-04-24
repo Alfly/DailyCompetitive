2f128d685d03660430aae26a16c676194d7c3301
alfly <alfly0023@gmail.com>
2023-04-24 14:18:35 +0800

    fix run script

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ cur_dir=$(pwd)
 g++ -o ${cur_dir}/$1 ${cur_dir}/$1.cpp -std=c++17 -O3
 
 if [ $# -eq 2 ]; then
-	if [ "$2" = "none" ]; then
+	if [ "$2" = "stdin" ]; then
 		${cur_dir}/$1
 	else
 		${cur_dir}/$1 < ${cur_dir}/$2.txt
