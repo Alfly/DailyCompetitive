2d3c22780ae0ead40e2b904de01eda2e29acc67f
alfly <alfly0023@gmail.com>
2024-03-14 21:55:47 +0800

    add union find

---
+++ work/written-test/20240309_美团/5.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <stack>
+#include <map>
+#include <unordered_map>
+using namespace std;
+using ll = long long;
+
+int n, m, q;
+unordered_map<int, int> p;
+map<pair<int, int>, int> mp;
+
+int Find(int x){
+    if (p[x] != x) p[x] = Find(p[x]);
+    return p[x];
+}
+
+void Union(int a,int b){
+    p[Find(a)] = Find(b);
+}
+
+int main() {
+    int a, b;
+    cin >> n >> m >> q;
+
+    for (int i = 1, u, v; i <= m; ++i) {
+        cin >> u >> v;
+        // 初始化
+        p[u] = u; p[v] = v;
+        // 记录边数
+        mp[{u,v}]++;
+        mp[{v,u}]++;
+    }
+
+    stack<tuple<int, int, int>> ops;
+    for(int i = 1, op, u, v; i <= q; ++i) {
+        cin >> op >> u >> v;
+        ops.push(make_tuple(op, u, v));
+        if (op == 1) {
+            mp[{u,v}]--;
+            mp[{v,u}]--;
+        }
+    }
+
+    for (auto& [edge, cnt] : mp) {
+        if (cnt > 0) {
+            Union(edge.first, edge.second);
+        }
+    }
+
+    stack<string> ans;
+    while (!ops.empty()) {
+        auto t = ops.top(); ops.pop();
+        int op = std::get<0>(t), u = std::get<1>(t), v = std::get<2>(t);
+        if (op == 1) {
+            mp[{u, v}]++;
+            mp[{v, u}]++;
+            if (mp[{u, v}] > 0) Union(u, v);
+        }
+        else{
+            if(p[u] == 0 || p[v] == 0){
+                ans.push("No");
+                continue;
+            }
+            ans.push(Find(u) == Find(v) ? "Yes" : "No");
+        }
+    }
+
+    while(!ans.empty()){
+        cout << ans.top() << "\n";
+        ans.pop();
+    }
+
+    return 0;
+}
