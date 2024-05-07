075aac4224ace2ff3569ae3fd5bbb111a646ffca
alfly <alfly0023@gmail.com>
2024-05-07 19:38:24 +0800

    refactor misc

--- Miscellaneous/fraction.cpp
+++ Miscellaneous/fraction.cpp
@@ -7,17 +7,17 @@ struct Fraction {
 
 // 最大公约数
 LL gcd(LL a, LL b){
-    if(b == 0) return a;
+    if (b == 0) return a;
     else return gcd(b, a % b);
 }
 
 // 约简
 Fraction reduction(Fraction f) {
-    if(f.down < 0) {
+    if (f.down < 0) {
         f.up = -f.up;
         f.down = -f.down;
     }
-    if(f.up == 0) {
+    if (f.up == 0) {
         f.down = 1;
     } else {
         int d = gcd(abs(f.up), abs(f.down));
@@ -54,13 +54,13 @@ Fraction div(Fraction a, Fraction b) {
 
 void show(Fraction r) {
     r = reduction(r);
-    if(r.up < 0) {
+    if (r.up < 0) {
         printf("-");
         r.up = abs(r.up);
     }
-    if(r.down == 1) {
+    if (r.down == 1) {
         printf("%lld", r.up);
-    } else if(r.up > r.down) {
+    } else if (r.up > r.down) {
         // 假分数: 分子大于或等于分母 -> 带分数: 非零自然数+真分数
         printf("%lld %lld/%lld", r.up / r.down, r.up % r.down, r.down);
     } else {
